8440f3f116bba07ab2561a9b9032d7e74b691903
annan <shy_annan@126.com>
2016-01-04 23:46:23 +0800

    x

---
+++ src/com/learning/filter/ErrorFilter.java
@@ -0,0 +1,51 @@
+package com.learning.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class UserFilter
+ */
+@WebFilter("/UserFilter")
+public class ErrorFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ErrorFilter() {
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		
+			System.out.println("Error Filter");
+			chain.doFilter(request, response);
+			
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
